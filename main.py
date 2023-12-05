9df2027cf2eb4512ce3e70501a12bc9d858bbf54
kirill221100 <kirulkacool@gmail.com>
2023-12-05 21:43:04 +0700

    req fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from security.config import get_config
 from routes.auth import auth_router
@@ -14,6 +15,13 @@ async def on_startup(app: FastAPI):
     yield
 
 app = FastAPI(debug=get_config().DEBUG, lifespan=on_startup)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(auth_router, prefix='/auth')
 app.include_router(order_router, prefix='/order')
 app.include_router(pizza_router, prefix='/pizza')
